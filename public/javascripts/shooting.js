82d895fa06380417ca3a94d056c267cbc87ea882
PqBenoit <benoit@monsieurdrive.com>
2015-03-18 22:27:21 +0100

    css stuff

--- public/javascripts/shooting.js
+++ public/javascripts/shooting.js
@@ -19,7 +19,11 @@ var Shooting = (function(my, Map, io, $)
 	{	
 		$('.hello').css({
 			paddingLeft: '150px',
-			paddingTop: '50px'
+		});
+
+		downbarHeight = $(window).height() - 65 - $('#movies-container').height();
+		$('.down-bar').css({
+			height: downbarHeight - 10 + 'px'
 		});
 
 		$('path').css({
@@ -43,9 +47,13 @@ var Shooting = (function(my, Map, io, $)
 			circle.attr('fill', '#2c9fc4');
 			circle.attr('stroke', 'none');
 
-			console.log('shootings placed');			
+			console.log('shootings placed');
 
 			$list.append('<li class="list-item">' + data.data.fields.titre + '</li>');
+
+			$(circle.node).click(function(){
+				my.openLi(data.data.fields.titre);
+			});
 
 			if(counter == 650){
 				$('path').animate({
@@ -68,10 +76,47 @@ var Shooting = (function(my, Map, io, $)
 				circle = Map.rsr.circle(pos.x, pos.y, 10);
 				circle.attr('fill', '#2c9fc4');
 				circle.attr('stroke', 'none');
+				circle.attr('index', i);
 			}
+
+			$('circle').click(function(){
+				console.log($(this));
+			});
 			console.log('new shootings placed');
+
+
 		});
 	};
+
+	/**
+	*  
+	* 
+	* 
+	*/
+	my.openLi = function(title, count)
+	{	
+		if(count){
+			$('.down-bar').empty();
+			var div = '<div class=\'li-click\'><h2>' + title + '</h2><p>' + count + ' lieux de tournage</p></div>'
+			$('.down-bar').append(div);
+			var marginTop = ($('.down-bar').height() / 2) - ($('.li-click').height()/2);
+			console.log(marginTop);
+			$('.li-click').css({
+				marginTop: marginTop
+			});
+		}
+		else {
+			$('.down-bar').empty();
+			var div = '<div class=\'li-click\'><h2>' + title + '</h2></div>'
+			$('.down-bar').append(div);
+			var marginTop = ($('.down-bar').height() / 2) - ($('.li-click').height()/2);
+			console.log(marginTop);
+			$('.li-click').css({
+				marginTop: marginTop
+			});	
+		}
+
+	}
 
 	/**
 	*  
@@ -88,6 +133,9 @@ var Shooting = (function(my, Map, io, $)
 			var title = $(this).html();
 			console.log(title);
 			my.socket.emit('titleClicked', {title: title});
+			setTimeout(function(){
+				my.openLi(title, $('circle').length);
+			}, 500);
 		});
 
 	}
